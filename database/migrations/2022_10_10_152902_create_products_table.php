22f2739a4082c1eb5f1c26813fe7869c7c97273c
hightt <konrad.duda121@gmail.com>
2022-10-11 09:40:41 +0200

    migration

---
+++ database/migrations/2022_10_10_152902_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('external_id')->unsigned()->index();
+            $table->longText('name');
+            $table->float('current_price');
+            $table->boolean('promotion');
+            $table->float('old_price');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2022_10_11_072148_create_price_histories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePriceHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('external_id')->unsigned();
+            $table->float('price');
+            $table->timestamps();
+
+            $table->foreign('external_id')
+            ->references('external_id')
+            ->on('products')
+            ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price_histories');
+    }
+}
