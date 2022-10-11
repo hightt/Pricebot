34347647c49cc43ea9f4f9dedea74e99ae6f642b
hightt <konrad.duda121@gmail.com>
2022-10-11 09:41:21 +0200

    PriceHistory model

---
+++ app/Models/PriceHistory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PriceHistory extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id', 'external_id', 'price', 'created_at', 'updated_at'];
+    protected $guarded = [];
+
+
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+    public function createPriceHistory(array $products)
+    {
+        $i = 0;
+        foreach ($products as $product) {
+            PriceHistory::create([
+                'external_id' => $product->external_id,
+                'price' => $product->current_price,
+            ]);
+            $i++;
+        }
+        echo "*** CREATED: " . $i . " HISTORY PRICES*** \n";
+    }
+}
