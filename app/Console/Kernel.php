909a266d464af6d263cd8a37e258ec23c1f1ee4e
hightt <konrad.duda121@gmail.com>
2022-10-14 19:10:53 +0200

    change cron time hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('products:update')->dailyAt('9:00');
+        $schedule->command('products:update')->dailyAt('6:00');
     }
 
     /**
