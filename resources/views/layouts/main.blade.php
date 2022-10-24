3f8170d0939f51cac293b52db983128fd2606aa9
hightt <konrad.duda121@gmail.com>
2022-10-24 10:59:45 +0200

    new info layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,78 +2,26 @@
 
 
 @section('content')
-<div class="col-lg-12 pt-4 pb-4 ps-0 pe-0">
-    <table id="mainTable" class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Produkt</th>
-                <th scope="col">Obecna cena (zł)</th>
-                <th scope="col">Promocja</th>
-                <th scope="col">Przeceniony (zł)</th>
-            </tr>
-        </thead>
-    </table>
-</div>
-<script>
-    $(document).ready(function() {
-        $('#mainTable').DataTable({
-            processing: true,
-            serverSide: true,
-            responsive: true,
-            ajax: {
-                url: "{{route('products.ajax')}}",
-                'headers': {
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                },
-            },
-            columns: [{
-                    data: 'external_id',
-                    responsivePriority: 1
-                },
-                {
-                    data: 'name',
-                    responsivePriority: 5,
-                    render: function(data, type, row) {
-                        var name = data;
-                        if (parseInt(data.length) > 95)
-                            name = data.substring(0, 95) + "...";
-                        return "<a class='product-name text-decoration-none' href='/products/" + row.id + "' " + "title='" + data + "'" + ">" + "<span class='product_name'>" + name + "</span>" + "</a>";
-                    }
-                },
-                {
-                    data: 'current_price',
-                    responsivePriority: 2
-                },
-                {
-                    data: 'promotion',
-                    responsivePriority: 3
-                },
-                {
-                    data: 'old_price',
-                    responsivePriority: 4
-                },
-            ],
-            paging: true,
-            order: [
-                [4, 'desc']
-            ],
-            info: false,
-            lengthChange: false,
-            pageLength: 15,
-            searching: true,
-            language: {
-                searchPlaceholder: "Wyszukaj produkty",
-                search: "",
-                paginate: {
-                    'previous': '<i class="fa-solid fa-chevron-left"></i>',
-                    'next': '<i class="fa-solid fa-chevron-right"></i>'
-                },
-            },
-            "sDom": '<"row view-filter"<"col-sm-12"<"pull-left"l><"pull-right"f><"clearfix">>>t<"row view-pager"<"col-sm-12"<"text-center"ip>>>'
-            // "dom": 'rtp'
 
-        });
-    });
-</script>
+<div class="col-lg-12" style="min-height: 50vh;">
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-8 align-middle">
+            <h3>Monitoring cen hurtowni <span class="text-warning">Dolina Noteci*</span></h3>
+            <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed molestie ligula. Vestibulum
+                ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis
+                in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque ut felis et arcu auctor dignissim a ac erat.
+                Fusce congue, quam in pretium accumsan, felis massa mollis arcu, sed semper mi sem quis metus.
+            </p>
+            <p class="text-muted" style="font-size: 10px;">*
+                Dolina Noteci posłużyła jako przykład zbioru produtków do analizy cen.
+                Niniejsza aplikacja internetowa nie ma charakteru konkurencji dla fimy Dolina Noteci :)
+            </p>
+        </div>
+        <div class="col-lg-4 text-center">
+            <i class="fa-solid fa-sack-dollar text-warning" style="font-size: 250px;"></i>
+        </div>
+    </div>
+
+</div>
+
 @endsection
