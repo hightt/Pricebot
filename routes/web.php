7d8f054605f14fcf7927d6e0059af25bd430e44b
hightt <konrad.duda121@gmail.com>
2022-10-24 10:59:59 +0200

    homecontroller routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
@@ -13,15 +14,14 @@ use Illuminate\Support\Facades\Artisan;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/products', ProductController::class);
 Route::get('/getProductsAjax', [ProductController::class, 'getProductsAjax'])->name('products.ajax');
-Route::get('/getDetailsAjax/{id}', [ProductController::class, 'getDetailsAjax'])->name('details.ajax');
-Route::get('/changedPrice', \App\Http\Controllers\ProductPriceController::class);
 
-Route::get('/', function(){
-    return redirect('/products');
-});
+Route::get('/getDetailsAjax/{id}', [ProductController::class, 'getDetailsAjax'])->name('details.ajax');
+Route::get('/changedPrice', \App\Http\Controllers\ProductPriceController::class)->name('products.changedPrice');;
+
 
 
 Route::get('/update_products/23398e43ab2ee0412f775adb8b52988a', [ProductController::class, 'cronJobUpdateProducts']);
