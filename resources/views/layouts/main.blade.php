d4bcfa12c2046f6a08375c3a222a25e715e404cc
hightt <konrad.duda121@gmail.com>
2022-10-26 15:30:13 +0200

    change the main view html structure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,21 +5,26 @@
 
 <div class="col-lg-12" style="min-height: 50vh;">
     <div class="row mt-5 mb-5">
-        <div class="col-lg-8 align-middle">
+        <div class="col-lg-8 align-middle mt-lg-5">
             <h3>Monitoring cen hurtowni <span class="text-warning">Dolina Noteci*</span></h3>
             <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed molestie ligula. Vestibulum
                 ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis
                 in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque ut felis et arcu auctor dignissim a ac erat.
                 Fusce congue, quam in pretium accumsan, felis massa mollis arcu, sed semper mi sem quis metus.
             </p>
-            <p class="text-muted" style="font-size: 10px;">*
-                Dolina Noteci posłużyła jako przykład zbioru produtków do analizy cen.
-                Niniejsza aplikacja internetowa nie ma charakteru konkurencji dla fimy Dolina Noteci :)
-            </p>
         </div>
         <div class="col-lg-4 text-center">
             <i class="fa-solid fa-sack-dollar text-warning" style="font-size: 250px;"></i>
         </div>
+    </div>
+    <div class="col-lg-12">
+        <a class="show-products btn btn-warning p-2" href="{{route('products.index')}}" role="button">Aktualne ceny produktów</a>
+    </div>
+    <div class="col-lg-12">
+        <p class="text-muted mt-4" style="font-size: 10px;">*
+            Dolina Noteci posłużyła jako przykład zbioru produktów do analizy cen.
+            Niniejsza aplikacja internetowa nie ma na celu działań konkurencyjnych wobec fimy Dolina Noteci :)
+        </p>
     </div>
 
 </div>
