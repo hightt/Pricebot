364ecfbe18a471f7cd2abbbc4eb6e0c9a9d51333
hightt <konrad.duda121@gmail.com>
2022-10-31 19:00:18 +0100

    Shorter function getAjaxPriceHistory

--- app/Http/Controllers/ProductPriceController.php
+++ app/Http/Controllers/ProductPriceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 
 class ProductPriceController extends Controller
 {
@@ -15,40 +16,18 @@ class ProductPriceController extends Controller
      */
     public function index()
     {
-        return view('layouts.changed_price_list');
+        return view('layouts.best_deal');
     }
 
-    public function getAjaxPriceHistory()
+    public function getAjaxPriceHistory(Product $product)
     {
-        $external_ids = DB::select(
-            DB::raw("SELECT DISTINCT external_id FROM price_histories ph  WHERE external_id IN (SELECT ph2.external_id
-        FROM price_histories ph2
-        GROUP BY ph2.external_id
-        HAVING (COUNT(DISTINCT ph2.price) = 1) = 0)")
-        );
+        // Get product with discount > $discountRange
+        $discountRange = 20;
 
+        $collection = collect(Product::all())->filter(function ($item) use ($discountRange){
+            return $item->discount > $discountRange;
+        })->sortBy('discount');
 
-        $results = [];
-        foreach ($external_ids as $val) {
-            $product = Product::where('external_id', $val->external_id)->first();
-            $results[$product->id] = [
-                'details' => $product->toArray(),
-                'price_history' => $product->pricehistories()->get()->toArray()
-            ];
-        }
-
-
-
-        foreach ($results as $price_history) {
-            foreach($price_history['price_history'] as $history) {
-                $results[$price_history['details']['id']]['labels'][] =  $history['created_at_formatted'];
-                $results[$price_history['details']['id']]['prices'][] =  $history['price'];
-            }
-
-        }
-        // echo "<pre>";
-        //     echo print_r($results);
-        // echo "</pre>";
-        return $results;
+        return $product->dataToChart($collection);
     }
 }
