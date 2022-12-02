721ea296446c3193f54820d2eacef3a861d18d12
hightt <konrad.duda121@gmail.com>
2022-12-02 11:39:49 +0100

    updateProducts.blade.php - mail template

---
+++ resources/views/emails/updateProducts.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Pricebot</title>
+</head>
+<body>
+    <h4>Dzisiejsza aktualizacja: </h4>
+    --------------------------------------------- <br>
+    <p>Wszystkich produktów: {{$content['total']}}</p>
+    <p>Utworzonych: {{$content['created']}}</p>
+    <p>Zaktualizowanych: {{$content['updated']}}</p>
+    <p>Włączonych: {{$content['enabled']}}</p>
+    <p>Wyłączonych: {{$content['disabled']}}</p>
+    --------------------------------------------- <br>
+
+</body>
+</html>
