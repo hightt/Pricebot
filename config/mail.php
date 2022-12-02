01127f7c99c00409f4f13baabca199451536480f
hightt <konrad.duda121@gmail.com>
2022-12-02 11:39:38 +0100

    mail.php - config

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'konrad.da121@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Pricebot'),
     ],
 
     /*
