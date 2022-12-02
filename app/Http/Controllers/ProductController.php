0fc565426fd93c88b2d30e7c0614dc91baf4e32e
hightt <konrad.duda121@gmail.com>
2022-12-02 09:58:48 +0100

    ProductController.php-refactor getProducts func

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,46 +19,10 @@ class ProductController extends Controller
         return view('layouts.content');
     }
 
-    public function getProductsAjax(Request $request)
+    public function getProductsAjax(Request $request, Product $product)
     {
-        $columnIndex =  $request->get('order')[0]['column'];
-        $columnName = $request->get('columns')[$columnIndex]['data'];
-        $columnSortOrder = $request->get('order')[0]['dir'];
-        $searchValue = $request->get('search')['value'];
-
-        $totalRecordsWithFilter = Product::select('count(*) as allcount')->where('name', 'like', '%' . $searchValue . '%')->where('active', 1)->count();
-
-        $records = Product::orderBy($columnName, $columnSortOrder)
-            ->where('products.name', 'like', '%' . $searchValue . '%')
-            ->where('products.active', 1)
-            ->select('products.*')
-            ->skip($request->get('start'))
-            ->take($request->get('length'))
-            ->get();
-
-        foreach ($records as $record) {
-            $record->current_price = number_format($record->current_price, 2, '.', '');
-            $record->old_price = number_format($record->old_price, 2, '.', '');
-
-            if ($record->promotion == 0) {
-                $record->promotion = "<i style='color: red;'>✕</i>";
-                $record->old_price = "<i style='color: red;'>✕</i>";
-            } else {
-                $record->promotion = "<i style='color: green;'>✓</i>";
-            }
-        }
-        return json_encode(
-            array(
-                "draw" => intval($request->get('draw')),
-                "iTotalRecords" => Product::all()->count(),
-                "iTotalDisplayRecords" => $totalRecordsWithFilter,
-                "aaData" => $records
-            )
-        );
+        return $product->getProductsAjax($request);
     }
-
-
-
 
     /**
      * Display the specified resource.
@@ -71,12 +35,9 @@ class ProductController extends Controller
         return view('layouts.details')->with('product', $product);
     }
 
-
-    public function getDetailsAjax(Product $product)
+    public function getProductAjax(Product $product)
     {
-        // Get product with discount > $discountRange
-        $discountRange = 20;
-        return $product->dataToChart($product);
+        return response()->json($product);
     }
 
 }
