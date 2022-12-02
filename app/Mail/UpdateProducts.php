eeff159d0d453d86874b27e1ca3bb0c8ed22071a
hightt <konrad.duda121@gmail.com>
2022-12-02 11:40:10 +0100

    UpdateProducts.php - mail sending module

---
+++ app/Mail/UpdateProducts.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateProducts extends Mailable
+{
+    use Queueable, SerializesModels;
+    protected $content;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject(sprintf('[%s] Pricebot logi', date("Y.m.d")))->view('emails.updateProducts')->with('content', $this->content);
+    }
+}
