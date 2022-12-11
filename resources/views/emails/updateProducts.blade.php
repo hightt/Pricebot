cada67d0db76e27330bb21c787b14a52c4025894
hightt <konrad.duda121@gmail.com>
2022-12-11 12:50:39 +0100

    Change logs nomenclature

--- resources/views/emails/updateProducts.blade.php
+++ resources/views/emails/updateProducts.blade.php
@@ -6,13 +6,12 @@
 </head>
 <body>
     <h4>Dzisiejsza aktualizacja: </h4>
+    <p>Wszystkich produktów pobranych z seeda: {{$content['total']}}</p>
+    <p>Dodano nowych: {{$content['created']}}</p>
+    <p>Zaktualizowano: {{$content['updated']}}</p>
+    <p>Aktywowano w bazie danych: {{$content['enabled']}}</p>
+    <p>Wyłączono w bazie danych: {{$content['disabled']}}</p>
     --------------------------------------------- <br>
-    <p>Wszystkich produktów: {{$content['total']}}</p>
-    <p>Utworzonych: {{$content['created']}}</p>
-    <p>Zaktualizowanych: {{$content['updated']}}</p>
-    <p>Włączonych: {{$content['enabled']}}</p>
-    <p>Wyłączonych: {{$content['disabled']}}</p>
-    --------------------------------------------- <br>
-
+    <?= sprintf("PS. Twój poziom essy (✿◠‿◠) dziś %s wynosi %d %%", date("d.m.Y"), rand(50, 100)) ?>
 </body>
 </html>
