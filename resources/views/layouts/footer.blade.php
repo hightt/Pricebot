73945ad2b85abf51eb79d87e30935d6628cfe9cd
hightt <konrad.duda121@gmail.com>
2022-12-19 14:03:12 +0100

    footer.blade.php - change footer structure

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,19 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 mt-3 border-top ">
-    <p class="col-md-4 mb-0 text-muted">&copy; 2022 Pricebot_v1, Inc</p>
+<footer class="py-3 my-4 mt-3 border-top ">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="col-md-12">
+        <span class="mb-0 text-muted">&copy; 2022 Pricebot_v1</span>
+      </div>
+      <div class="col-md-12">
+        <span class="mb-0 text-muted">Kontakt: konrad.duda121@gmail.com</span>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <ul class="nav justify-content-end d-block">
+        <li class="nav-item text-end"><a href="{{route('home')}}" class="nav-link px-2 text-muted">Strona główna</a></li>
+        <li class="nav-item text-end"><a href="{{route('products.index')}}" class="nav-link px-2 text-muted">Produkty</a></li>
+      </ul>
+    </div>
+  </div>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-    </a>
-
-    <ul class="nav col-md-4 justify-content-end d-block">
-      <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-muted">Strona główna</a></li>
-      <li class="nav-item"><a href="{{route('products.index')}}" class="nav-link px-2 text-muted">Produkty</a></li>
-    </ul>
-  </footer>
+</footer>
