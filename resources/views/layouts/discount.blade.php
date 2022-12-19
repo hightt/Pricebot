b42726139be00b2372051c62242254ba9a54bc95
hightt <konrad.duda121@gmail.com>
2022-12-19 14:02:51 +0100

    discount.blade.php-create table with biggest promo

--- resources/views/layouts/discount.blade.php
+++ resources/views/layouts/discount.blade.php
@@ -1,158 +1,45 @@
 @extends('index')
 @section('content')
 <script type="text/javascript" src="{{ asset('libraries/chart.js') }}"></script>
-
-<div id="root" class="container">
-    <div id="chartBox" cass="row">
-        <h3 class="chartTitle text-center"></h3>
-
-    </div>
-
-
-
-    <div class="d-none">
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Button with data-bs-target
-        </button>
-        <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-                <table  id="tableTemplate" class="table d-none">
-                    <thead>
-                        <tr>
-                            <th class="text-center" scope="col">Data</th>
-                            <th class="text-center" scope="col">Cena</th>
-                        </tr>
-                    </thead>
-                    <tbody id="tableTemplateBody">
-                        <tr id="tableTemplateTr" class="d-none">
-                            <td class="date">1</td>
-                            <td class="price">1</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-
-
-
-
-
+<div class="alert alert-info" role="alert">
+    Produkt wyświetlany w tej sekcji musi spełniać następujące kryteria:
+    <ul>
+        <li>Produkt musi być na promocji.</li>
+        <li>Promocja musi uwzględniać zniżkę wielkości minimum 20%.</li>
+        <li>Zaszła zmiana w cenie produktu w ciągu ostatnich 30 dni (wykluczenie fake'owych promocji).</li>
+        <li>Produkt musi mieć podaną starą cenę, produkty mające promocję bez starej ceny nie są uwzględniane.</li>
+    </ul>
 </div>
-<script>
-    $(document).ready(function() {
+<div id="root" class="container">
 
-        function getProducts() {
-            $.ajax({
-                url: "{{route('discount.biggest')}}",
-                success: function(response) {
-                    drawChart(response.result);
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-
-            });
-        }
-
-        function drawChart(products) {
-            let i = 0;
-            $.each(products, function(key, product) {
-                let canvas = "<canvas id='chart" + i + "'></canvas>";
-                let box = $("#chartBox").clone();
-                let tableCollapse = "";
-                let tableTemplateBox = $("#tableTemplate").clone().removeClass("d-none");
-
-                box.find(".chartTitle").html("<a class='discount-title' href='/products/ " + product.id + "'>" + product.name + "</a>");
-                box.append(canvas).append("<hr>");
-
-                drawTable(product, tableTemplateBox);
-                box.append(tableTemplateBox);
-
-                $("#root").append(box);
-                createChart(product, "chart" + i, i);
-                i++;
-            });
-        }
-
-        function drawTable(product, table) {
-            var i = 0;
-            $.each(product.axis_data.oxAxis, function(key, value) {
-                if (i % 14 == 0) {
-                    let oxAxis = value; // date
-                    let oyAxis = product.axis_data.oyAxis[key]; //price
-
-                    let tr = table.find("#tableTemplateTr").clone().removeClass('d-none');
-                    tr.removeAttr("id");
-                    tr.find('.date:first').text(oyAxis);
-                    tr.find('.price:first').text(oxAxis + " zł");
-                    // console.log(tr);
-                    tr.appendTo(table.find("#tableTemplateBody"));
-
-                }
-                i++;
-
-            });
-        }
-
-        function createChart(product, chartName, chartNum) {
-            const data = {
-                labels: product.axis_data.oyAxis,
-                datasets: [{
-                    label: "Cena: ",
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: product.axis_data.oxAxis
-                }]
-            };
-
-            const config = {
-                type: 'line',
-                data: data,
-                options: {
-                    plugins: {
-                        legend: {
-                            display: false,
-                            position: 'bottom',
-                            labels: {
-                                color: 'black',
-
-                            },
-                        },
-                        title: {
-                            display: true,
-                            text: "Rys. " + ++chartNum + " " + product.name,
-                            position: 'bottom'
-
-                        }
-                    },
-                    scales: {
-                        y: {
-                            title: {
-                                display: true,
-                                text: "Kwota (zł)"
-                            }
-                        },
-                        x: {
-                            title: {
-                                display: true,
-                                text: "Data (dzień - miesiąc - rok)"
-                            }
-                        }
-                    }
-                }
-            };
-
-            const myChart = new Chart(
-                document.getElementById(chartName),
-                config
-            );
-        }
+    @if(count($products) > 1)
+    @foreach ($products as $product)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Produkt</th>
+                <th>Zniżka cenowa</th>
+                <th>Procent zniżki</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><a class="product_name" href="{{ route('products.show', $product->id) }}">{{$product->name}}</a></td>
+                <td>{{sprintf("%.2f zł", $product->discount['price'])}}</td>
+                <td>{{sprintf("%.2f %%", $product->discount['percentage'])}}</td>
+            </tr>
+        </tbody>
+    </table>
+    @endforeach
+    @else
+    <h4 class="text-center">Brak produktów na promociji</h4>
+    @endif
 
 
-        getProducts();
-    });
-</script>
+    <div class="d-flex justify-content-center">
+        {{$products->links()}}
+    </div>
+</div>
+
 
 @endsection
