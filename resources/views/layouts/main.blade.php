c798c5acdaf5e4949a72650a4075b3136ba7462b
hightt <konrad.duda121@gmail.com>
2022-12-29 11:52:41 +0100

    main.blade.php - main site refactoring

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,31 +2,51 @@
 
 
 @section('content')
-
-<div class="col-lg-12" style="min-height: 75vh;">
-    <div class="row mt-5 mb-5">
-        <div class="col-lg-8 align-middle mt-lg-5">
-            <h3>Monitoring cen hurtowni <span class="text-warning">Dolina Noteci*</span></h3>
-            <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed molestie ligula. Vestibulum
-                ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis
-                in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque ut felis et arcu auctor dignissim a ac erat.
-                Fusce congue, quam in pretium accumsan, felis massa mollis arcu, sed semper mi sem quis metus.
+<div class="row" style="min-height: 75vh;">
+    <div class="col-lg-6 mt-lg-5">
+        <div class="col-lg-12">
+            <h2>Monitoring cen hurtowni <span class="text-warning">Dolina Noteci*</span></h2>
+            <h5 style="text-align: justify;">
+                Pricebot tworzy historię cen produktów, a także dokonuje analizy wiarygodności promocji produktów wyświetlanych w hurtowni. Dane przedstawione są na interaktywnych wykresach umożliwiających szybkie i przyjemne pozyskiwanie informacji.
+            </h5>
+            <ul class="fa-ul">
+                <li>
+                    <span class="fa-li">
+                        <i class="fa-solid fa-check" style="color: green"></i>
+                    </span>
+                    Historia cen
+                </li>
+                <li>
+                    <span class="fa-li">
+                        <i class="fa-solid fa-check" style="color: green"></i>
+                    </span>
+                    Weryfikacja wiarygodności promocji
+                </li>
+                <li>
+                    <span class="fa-li">
+                    <i class="fa-solid fa-person-digging"></i>
+                    </span>
+                    <span class="text-muted">Porównywanie cen produktów</span>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-12">
+            <a class="show-products btn btn-warning pt-2 pb-2 ps-4 pe-4 mt-4" href="{{route('products.index')}}" role="button">Zobacz aktualne ceny produktów</a>
+            <p class="text-muted mt-2" style="font-size: 10px;">*
+                Dolina Noteci posłużyła jako przykład zbioru produktów do analizy cen.
+                Niniejsza aplikacja internetowa nie ma na celu działań konkurencyjnych wobec fimy Dolina Noteci.
             </p>
         </div>
-        <div class="col-lg-4 text-center">
-            <i class="fa-solid fa-sack-dollar text-warning" style="font-size: 250px;"></i>
-        </div>
     </div>
-    <div class="col-lg-12">
-        <a class="show-products btn btn-warning pt-2 pb-2 ps-4 pe-4" href="{{route('products.index')}}" role="button">Zobacz aktualne ceny produktów</a>
+    <div class="col-lg-6">
+        <!-- 
+            Img author: Matthew Jedrzejewski
+            https://dribbble.com/MattJedrzejewski
+        -->
+        <img class="img-fluid" src="{{ asset('images/website.gif') }}">
     </div>
-    <div class="col-lg-12">
-        <p class="text-muted mt-4" style="font-size: 10px;">*
-            Dolina Noteci posłużyła jako przykład zbioru produktów do analizy cen.
-            Niniejsza aplikacja internetowa nie ma na celu działań konkurencyjnych wobec fimy Dolina Noteci :)
-        </p>
-    </div>
-
 </div>
+
+
 
 @endsection
