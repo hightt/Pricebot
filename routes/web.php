653018ee5ac73e67ede16345c586aed33ab2da7f
hightt <konrad.duda121@gmail.com>
2023-01-02 17:39:43 +0100

    web.php - compare products routing

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductAnaliseController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductPriceController;
-use App\Http\Controllers\WebScrapperController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +14,25 @@ use Illuminate\Support\Facades\Artisan;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/products', ProductController::class)->only(['index', 'show']);
 
-Route::group(['prefix'=>'getProductsAjax', 'as'=>'products.ajax.'], function(){
+Route::group(['prefix' => 'getProductsAjax', 'as' => 'products.ajax.'], function () {
     Route::get('/', [ProductController::class, 'getProductsAjax'])->name('all');
     Route::get('/{product}', [ProductController::class, 'getProductAjax'])->name('one');
-    
 });
 
-Route::group(['prefix'=>'discount', 'as'=>'discount.'], function(){
+Route::group(['prefix' => 'discount', 'as' => 'discount.'], function () {
     Route::get('/', [ProductAnaliseController::class, 'index'])->name('index');
     Route::get('/bestDeal', [ProductAnaliseController::class, 'bestDealProductsAjax'])->name('biggest');
+});
+
+Route::get('/compare', [ProductAnaliseController::class, 'compareIndex'])->name('compare.index');
+
+Route::group(['prefix' => 'compare', 'as' => 'compare.'], function () {
+    Route::get('/addProduct', [ProductAnaliseController::class, 'addProductToCompare'])->name('add');
+    Route::get('/removeProduct/{id}', [ProductAnaliseController::class, 'removeProductFromSession'])->name('remove');
+    Route::get('/getProducts', [ProductAnaliseController::class, 'getProductsFromSession'])->name('get');
 });
