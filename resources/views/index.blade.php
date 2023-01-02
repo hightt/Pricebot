d144ee80559ce1aee7ff7cceeaf135e7dd8453cf
hightt <konrad.duda121@gmail.com>
2023-01-02 17:33:26 +0100

    index.blade.php-remove reduntant libraries includes, add script with tooltip and messages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,60 +4,65 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pricebot_v1</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Pricebot</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/DataTables-1.12.1/css/dataTables.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/AutoFill-2.4.0/css/autoFill.bootstrap5.min.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/Buttons-2.2.3/css/buttons.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/ColReorder-1.5.6/css/colReorder.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/DateTime-1.1.2/css/dataTables.dateTime.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/FixedColumns-4.1.0/css/fixedColumns.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/FixedHeader-3.2.4/css/fixedHeader.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/KeyTable-2.7.0/css/keyTable.bootstrap5.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/Responsive-2.3.0/css/responsive.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/RowGroup-1.2.0/css/rowGroup.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/RowReorder-1.2.8/css/rowReorder.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/Scroller-2.0.7/css/scroller.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/SearchBuilder-1.3.4/css/searchBuilder.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/SearchPanes-2.0.2/css/searchPanes.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/Select-1.4.0/css/select.bootstrap5.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('libraries/DataTables/StateRestore-1.1.1/css/stateRestore.bootstrap5.css') }}" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/jQuery-3.6.0/jquery-3.6.0.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/DataTables-1.12.1/js/jquery.dataTables.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/DataTables-1.12.1/js/dataTables.bootstrap5.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/Responsive-2.3.0/js/dataTables.responsive.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/Responsive-2.3.0/js/responsive.bootstrap5.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/AutoFill-2.4.0/js/dataTables.autoFill.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/AutoFill-2.4.0/js/autoFill.bootstrap5.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/Buttons-2.2.3/js/dataTables.buttons.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/Buttons-2.2.3/js/buttons.bootstrap5.js') }}"></script>
     <script type="text/javascript" src="{{ asset('libraries/DataTables/Buttons-2.2.3/js/buttons.colVis.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/ColReorder-1.5.6/js/dataTables.colReorder.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/DateTime-1.1.2/js/dataTables.dateTime.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/FixedColumns-4.1.0/js/dataTables.fixedColumns.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/FixedHeader-3.2.4/js/dataTables.fixedHeader.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/KeyTable-2.7.0/js/dataTables.keyTable.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/RowGroup-1.2.0/js/dataTables.rowGroup.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/RowReorder-1.2.8/js/dataTables.rowReorder.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/Scroller-2.0.7/js/dataTables.scroller.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/SearchBuilder-1.3.4/js/dataTables.searchBuilder.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/SearchBuilder-1.3.4/js/searchBuilder.bootstrap5.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/SearchPanes-2.0.2/js/dataTables.searchPanes.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/SearchPanes-2.0.2/js/searchPanes.bootstrap5.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/Select-1.4.0/js/dataTables.select.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/StateRestore-1.1.1/js/dataTables.stateRestore.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('libraries/DataTables/StateRestore-1.1.1/js/stateRestore.bootstrap5.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.cookie.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("body").tooltip({
+                selector: '[data-toggle=tooltip]'
+            });
 
 
+        });
+
+        function showMessage(message = "", status = "") {
+            $('#msg').addClass('alert-' + status).show();
+            $('#msg').children('span').text(message);
+            $('#msg').delay(2000).fadeOut('slow');
+        }
+    </script>
+
+
+    <div id="msg" class="alert msg-alert" role="alert" style="display:none;">
+        <span></span>
+    </div>
+
+    @if (Session::has('alert'))
+    <?php
+    $msg = Illuminate\Support\Facades\Session::get('alert');
+    Illuminate\Support\Facades\Session::forget('alert');
+    ?>
+    <script>
+        showMessage("{{$msg['message']}}", "{{$msg['status']}}")
+    </script>
+
+    @endif
     <div class="container">
         <div class="row">
             @include('layouts.header')
@@ -69,8 +74,6 @@
             @include('layouts.footer')
         </div>
     </div>
-
-
 </body>
 
 </html>
